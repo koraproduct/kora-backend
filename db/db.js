5b2e47196f8f525f9fd4de5c5087f0a936164df7
koraproduct <koraproduct@gmail.com>
2025-04-09 05:33:00 +0530

    db password changes

--- db/db.js
+++ db/db.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-//console.log('DB URL:', process.env.SUPABASE_DB_URL); // Debug
 const pool = new Pool({
   connectionString: process.env.SUPABASE_DB_URL,
-  //connectionString: 'postgresql://postgres.cfbqssfdjenmmdjudmgt:smKuLd5MCpzmQzCL@aws-0-ap-south-1.pooler.supabase.com:6543/postgres',
   ssl: { rejectUnauthorized: false },
 });
 
